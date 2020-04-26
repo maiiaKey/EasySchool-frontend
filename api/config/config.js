a4676ff20108d7e5e86af433322ee4d760e39547
maiiaKey <59762050+maiiaKey@users.noreply.github.com>
2020-04-26 20:41:05 +0200

    initial commit

--- api/config/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    secret: 'j8tZ5Gmz9mqDQpUa3opwhnsFWAVJztGJ3927T6v8iMLYZe3t'
-  };
--- api/config/db.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: 'localhost',
-    database: 'es',
-    user: 'es',
-    password: 'dACsXcgC66mMEescgdKYBRPbHmuZCTby5pzPGsEHJYgk255b',
-    port: 5432
-}
